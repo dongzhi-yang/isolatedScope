b963c3dc282257361b2eb15d5dc68995eb82a14b
dongzhi-yang <dongzhichka@gmail.com>
2016-04-10 22:20:52 +0800

    changed to controllerAs syntax

--- client/isolatedScope.js
+++ client/isolatedScope.js
@@ -6,35 +6,45 @@ angular.module("isolatedScope",[angularMeteor])
     
     //define controller here and use $inject so that strict can work.
     var controller1=function($scope){
-        $scope.changeFunctionFromInside=()=>{
+
+        this.changeFunctionFromInside=()=>{
             $scope.functionBind=(text)=>{
              alert("this function belongs to the isolated scope only. text parameter is:"+text);
+           
          };
+        
         };
+        //attach all properties of $scope to this.this won't work for primitive types like string , number
+         Object.keys($scope).forEach((key, idx)=> {
+          this[key]=$scope[key];           
+         }); 
 
      };
-    controller1.$inject = ["$scope", "$http", "$filter"];  
+    controller1.$inject = ["$scope"];  
     return {
       restrict: "EA",
       scope: {
         textBind: "@attrTextBind",
         twoWayBind: "=attrTwoWayBind",
         functionBind: "&attrFunctionBind",
-        objectBind: "&attrObjectBind"
+        objectBind: "&attrObjectBind",
+        twoWayBindObject:"=attrTwoWayBindObject"
       },
       templateUrl:'client/isolatedScope.html',
-     controller:controller1
+     controller:controller1,
+     controllerAs:"iso"
     };
 })
-.controller("isolatedScopeCtrl",["$scope",function($scope){
-                                                                $scope.text = "initial text";
-                                                                $scope.alertTextFunction =(text)=>{
-                                                                    alert("this function belongs to the wrapper scope. text parameter is:"+text);
-                                                                }
-                                                                $scope.obj={
-                                                                    "text":"obj.text"
-                                                                };
-                                                            }]);
+.controller("isolatedScopeCtrl",function(){
+  
+this.text = "initial text";
+this.alertTextFunction =(text)=>{
+                 alert("this function belongs to the wrapper scope. text parameter is:"+text);
+             }
+ this.obj={
+        "text":"obj.text"
+    };
+});
 
 
 
